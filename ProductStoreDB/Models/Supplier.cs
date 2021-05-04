a7b59685600e31d10d26cbd0dfdbf034041847bf
Miroslava <miroslavaroshchina@gmail.com>
2021-05-04 20:24:07 +0300

    Добавьте файлы проекта.

---
+++ ProductStoreDB/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductStoreDB.Models
+{
+    class Supplier
+    {
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public int ProductTupeID { get; set; }
+        public string Logo { get; set; }
+
+    }
+}
